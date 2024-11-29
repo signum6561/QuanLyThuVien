711f9c9fae1381c814070bc3a5fe93dd54469de3
signum6561 <quiocdat777@gmail.com>
2024-11-29 23:13:41 +0700

    update master

--- src/entity/SachThamKhao.java
+++ src/entity/SachThamKhao.java
@@ -37,4 +37,10 @@ public class SachThamKhao extends Sach {
     public void setThue(double thue) {
         this.thue = thue;
     }
+
+    @Override
+    public String toString() {
+        return "SachThamKhao [maSach=" + maSach + ", thue=" + thue + ", ngayNhap=" + ngayNhap + ", donGia=" + donGia
+                + ", soLuong=" + soLuong + ", nhaXuatBan=" + nhaXuatBan + ", thanhTien=" + tinhThanhTien() + "]";
+    }
 }
