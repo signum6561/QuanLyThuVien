66ff5aa244d89308522539589786b1a234fb59ce
signum6561 <quiocdat777@gmail.com>
2024-12-05 11:33:27 +0700

    fix message print in GitTest

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -2,6 +2,6 @@ package test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 123");
     }
 }
