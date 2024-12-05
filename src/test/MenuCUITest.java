db0018c5dd503f8af24046b9d8eae1e9284b9751
signum6561 <quiocdat777@gmail.com>
2024-12-05 11:31:06 +0700

    add GitTest file

---
+++ src/test/MenuCUITest.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Scanner;
+
+import ui.MenuCUI;
+import ui.util.Printer;
+
+public class MenuCUITest {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
