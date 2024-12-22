acc65e2cb0564d758fc44e06ecf3b721758e8f96
signum6561 <quiocdat777@gmail.com>
2024-12-22 22:24:22 +0700

    update mysql setup

--- init.sql
+++ init.sql
@@ -1,12 +1,2 @@
 create database if not exists sach;
 use sach;
-
-create table if not exists sua (
-    id int not null auto_increment,
-    ten_sua varchar(255) not null,
-    ma_sua varchar(255) not null,
-    ngay_thanh_toan varchar(255) not null,
-    ngay_thanh_toan_tiem varchar(255) not null,
-    so_sach varchar(255) not null,
-    primary key (id)
-);
--- src/database/mapper/SachMapper.java
+++ src/database/mapper/SachMapper.java
@@ -1,5 +1,12 @@
 package database.mapper;
 
-public class SachMapper {
+import java.sql.ResultSet;
 
+import application.entity.Sach;
+
+public class SachMapper {
+    public static Sach getSach(ResultSet rs) {
+        Sach sach = null;
+        return sach;
+    }
 }
